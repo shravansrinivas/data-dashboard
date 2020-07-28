81339fac04c541a0fadb187ac708c5b36bcc2214
Shravan Srinivas <shravansrinivasgs56@gmail.com>
2020-07-28 22:24:30 +0530

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+import App from './Components/Main';
+import 'jquery';
+import 'popper.js';
+//import Sunburst from 'react-sunburst-d3-v4';
+import 'bootstrap';
+// import { AgGridReact } from 'ag-grid-react';
+
+// import 'ag-grid-community/dist/styles/ag-grid.css';
+// import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
+
+// render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
